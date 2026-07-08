b62f6ea12f5710033ac6220d2983574d9758220a
Zakharov Andrey <37038505+ZakharovAndrew@users.noreply.github.com>
2026-07-09 00:22:36 +0300

    Create update.php

---
+++ src/views/category/update.php
@@ -0,0 +1,19 @@
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+
+$this->title = 'Update Category: ' . $model->name;
+$this->params['breadcrumbs'][] = ['label' => 'Categories', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = 'Update';
+?>
+
+<div class="category-update">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+        'actionUrl' => Url::to(['update', 'id' => $model->id]),
+    ]) ?>
+</div>
